80c1c127fbba9ac0a6bb332d8123597804f4e3e0
coefvo <verticalorigin.com>
2021-05-30 20:28:53 +0100

    updates

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -69,8 +69,9 @@ def login():
             flash('Invalid username or password')
             app.logger.warning('Failed login attempt.')
             return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
         app.logger.info('Successful User Login')
+        login_user(user, remember=form.remember_me.data)
+        #app.logger.info('Successful User Login')
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
